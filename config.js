ccb0f1573f9082be724380f7f437eeb714cd3bae
farhad <farhadino@gmail.com>
2016-11-30 13:45:34 +0100

    Update config.js
    oops...

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key: 'kpTO6L3tPBP1gO7tAKf3v13J4',  
-  consumer_secret: 'dJv4JKgFAvOHySGtGSEQkae7aWMts30JomfsbBHlkebujwUyAN',
-  access_token: '803574435353600000-s1omHFPzRaqjITA5Hz1Kkom0Wjfn5dJ',  
-  access_token_secret: 'ANZ7xH9E68TXqixqO46HBIYFYvm84c42CEqnKegVeUqZm'
+  consumer_key: '',  
+  consumer_secret: '',
+  access_token: '',  
+  access_token_secret: ''
 }
