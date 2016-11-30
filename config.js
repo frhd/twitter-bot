7088c51978c824da52685a095d986995751aca9e
farhad.omid <farhad.omid@eddyson.de>
2016-11-30 12:37:11 +0100

    yes

---
+++ config.js
@@ -0,0 +1,12 @@
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: '',  
+  consumer_secret: '',
+  access_token: '',  
+  access_token_secret: ''
+}
